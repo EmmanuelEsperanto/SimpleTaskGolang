b097c305e8698037df3080b026bc8f1f224cc680
EmmanuelEsperanto <jobamhad@gmail.com>
2025-04-13 14:50:35 +0400

    Made create tasks and read list of task

--- main.go
+++ main.go
@@ -1,15 +1,62 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+const (
+	E_Create = "create"
+	E_Read   = "read"
+	E_Update = "update"
+	E_Delete = "delete"
 )
 
-func main() {
+type TaskData struct {
+	TaskName        string
+	TaskDescription string
+}
 
-	s := "gopher2323"
-	fmt.Println("Hello and welcome, %s!", s)
+var Tasks = make([]TaskData, 0, 3)
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+func PrintBaseMessage() {
+	fmt.Printf("Enter your command (%s, %s, %s, %s): \n", E_Create, E_Read, E_Update, E_Delete)
+}
+
+func CreateTask(TaskName string, TaskDescription string) {
+	Tasks = append(Tasks, TaskData{TaskName, TaskDescription})
+	fmt.Printf("Created task name is:  %s \n", TaskName)
+}
+
+func PrintEnterTaskName() {
+	fmt.Println("Enter task name")
+}
+
+func PrintEnterTaskDescription() {
+	fmt.Println("Enter task description")
+}
+
+func PrintTaskNames() {
+	fmt.Print("List of tasks: \n")
+	for _, task := range Tasks {
+		fmt.Printf("| %s: |\n--%s", task.TaskName, task.TaskDescription)
 	}
+}
+
+func main() {
+	input := ""
+	PrintBaseMessage()
+	for {
+		fmt.Scan(&input)
+		switch input {
+		case E_Create:
+			PrintEnterTaskName()
+			fmt.Scan(&input)
+			TaskName := input
+			PrintEnterTaskDescription()
+			fmt.Scan(&input)
+			CreateTask(TaskName, input)
+		case E_Read:
+			PrintTaskNames()
+		}
+		input = ""
+	}
+
 }
