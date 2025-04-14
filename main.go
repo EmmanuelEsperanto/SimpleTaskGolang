216e3c4017d7e8cdd68f486fb6e5b1fa6d6a5469
EmmanuelEsperanto <jobamhad@gmail.com>
2025-04-14 13:17:55 +0400

    Add cancel command and delete operation

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var Tasks = make([]TaskData, 0, 3)
 func CreateTask(TaskName string, TaskDescription string) {
 	if CheckTaskExists(TaskName) {
 		fmt.Print("Error creating task, reason: ")
-		PrintTaskExists()
+		PrintTaskExistsMessage()
 		return
 	}
 	Tasks = append(Tasks, TaskData{TaskName, TaskDescription})
@@ -47,57 +47,82 @@ func UpdateTaskName(NewTaskName, OldTaskName string) {
 		}
 	}
 	fmt.Print("Error updating task name, reason: ")
-	PrintTaskExists()
+	PrintTaskExistsMessage()
 }
 
-func DeleteTask(TaskName string) {
+func DeleteTask(TaskName string) bool {
 	for i, task := range Tasks {
 		if task.TaskName == TaskName {
 			Tasks[i] = Tasks[len(Tasks)-1]
 			Tasks = Tasks[:len(Tasks)-1]
+			PrintTaskDeletedMessage()
+			return true
 		}
 	}
+	PrintTaskNoExistsMessage()
+	return false
 }
 
-func PrintCancelMessage() {
-	fmt.Println("You can cancel operation if type: !cancel")
-}
-
-func PrintTaskNoExists() {
-	fmt.Println("Task does not exist")
-}
-
-func PrintTaskExists() {
-	fmt.Println("Task exists")
-}
-
-func PrintTaskNames() {
+func PrintTaskNamesMessage() {
+	if len(Tasks) == 0 {
+		fmt.Println("No tasks found")
+		return
+	}
 	fmt.Print("List of tasks: \n")
 	for i, task := range Tasks {
 		fmt.Printf("[%v]| %s: |\n	{--%s--}\n", i+1, task.TaskName, task.TaskDescription)
 	}
 }
 
+func PrintTaskDeletedMessage() {
+	fmt.Println("Task deleted")
+}
+
+func PrintTaskNameDeleteMessage() {
+	fmt.Println("Enter delete task name: ")
+	PrintCancelMessage()
+}
+
+func CheckCancel(command *string) bool {
+	if *command == E_Cancel {
+		*command = ""
+		return true
+	}
+	return false
+}
+
+func PrintCancelMessage() {
+	fmt.Println("You can cancel operation if type: !cancel")
+}
+
+func PrintTaskNoExistsMessage() {
+	fmt.Println("Task does not exist")
+}
+
+func PrintTaskExistsMessage() {
+	fmt.Println("Task exists")
+}
+
 func PrintBaseMessage() {
 	fmt.Printf("Enter your command (%s, %s, %s, %s): \n", E_Create, E_Read, E_Update, E_Delete)
 }
 
-func PrintEnterTaskName() {
+func PrintEnterTaskNameMessage() {
 	fmt.Println("Enter task name")
 	PrintCancelMessage()
 }
 
-func PrintEnterNewTaskName() {
+func PrintEnterNewTaskNameMessage() {
 	fmt.Println("Enter new task name")
 	PrintCancelMessage()
 }
 
-func PrintEnterTaskDescription() {
+func PrintEnterTaskDescriptionMessage() {
 	fmt.Println("Enter task description")
 	PrintCancelMessage()
 }
 
-func PrintLittleTaskName() {
+func PrintLittleTaskNameMessage() {
 	fmt.Println("Please enter name more than 3 symbols")
 }
 
@@ -115,7 +140,6 @@ func main() {
 	input := ""
 
 	for {
-
 		if len(input) == 0 {
 			PrintBaseMessage()
 			_, err := fmt.Scan(&input)
@@ -127,39 +151,36 @@ func main() {
 		}
 		switch input {
 		case E_Create:
-			//BackToStartOfCreate:
-			PrintEnterTaskName()
+			PrintEnterTaskNameMessage()
 			_, err1 := fmt.Scan(&input)
 			if err1 != nil {
 				PrintErrorMessage()
 				return
 			}
-			if input == E_Cancel {
-				input = ""
+			if CheckCancel(&input) {
 				break
 			}
+
 			if len(input) < 3 {
-				PrintLittleTaskName()
+				PrintLittleTaskNameMessage()
 				input = E_Create
 				break
-				//goto BackToStartOfCreate
 			}
 			TaskName := input
-			PrintEnterTaskDescription()
+			PrintEnterTaskDescriptionMessage()
 			_, err2 := fmt.Scan(&input)
 			if err2 != nil {
 				PrintErrorMessage()
 				return
 			}
-			if input == E_Cancel {
-				input = ""
+			if CheckCancel(&input) {
 				break
 			}
 			CreateTask(TaskName, input)
 			input = ""
 
 		case E_Read:
-			PrintTaskNames()
+			PrintTaskNamesMessage()
 			input = ""
 		case E_Update:
 			PrintSelectTaskToUpdate()
@@ -168,45 +189,53 @@ func main() {
 				PrintErrorMessage()
 				return
 			}
-			if input == E_Cancel {
-				input = ""
+			if CheckCancel(&input) {
 				break
 			}
 			if len(input) < 3 {
-				PrintLittleTaskName()
+				PrintLittleTaskNameMessage()
 				input = E_Update
 				break
 			}
 			if !CheckTaskExists(input) {
-				PrintTaskNoExists()
+				PrintTaskNoExistsMessage()
 				input = E_Update
 				break
 			}
 			OldTaskName := input
-			PrintEnterNewTaskName()
+			PrintEnterNewTaskNameMessage()
 			_, err2 := fmt.Scan(&input)
 			if err2 != nil {
 				PrintErrorMessage()
 				return
 			}
-			if input == E_Cancel {
-				input = ""
+			if CheckCancel(&input) {
 				break
 			}
 			if len(input) < 3 {
-				PrintLittleTaskName()
+				PrintLittleTaskNameMessage()
 				input = E_Update
 				break
 			}
 			if len(Tasks) > 1 && CheckTaskExists(input) {
-				PrintTaskExists()
+				PrintTaskExistsMessage()
 				input = E_Update
 				break
 			}
 			UpdateTaskName(input, OldTaskName)
 			input = ""
 		case E_Delete:
-
+			PrintTaskNameDeleteMessage()
+			_, err := fmt.Scan(&input)
+			if err != nil {
+				PrintErrorMessage()
+				return
+			}
+			if CheckCancel(&input) {
+				break
+			}
+			DeleteTask(input)
+			input = ""
 		case E_Cancel:
 			input = ""
 			break
